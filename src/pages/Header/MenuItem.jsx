6e8f1beafdb3272ea94dfdfe2149ec26c49a32e2
dongyu-youn <donghe1472@naver.com>
2024-08-28 13:06:04 +0900

    [Feat] 리팩토링

--- src/pages/Header/MenuItem.jsx
+++ src/pages/Header/MenuItem.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function MenuItem({ name }) {
   return (
     <li className="p-3 cursor-pointer font-customFont text-2xl text-white transition duration-300 ease-in-out transform  hover:text-pink-800 hover:scale-110 group-hover:text-black">
-      <span className="stroke-black stroke-1 font-bold"> {name}</span>
+      <span className="stroke-black stroke-3 font-bold"> {name}</span>
     </li>
   );
 }
