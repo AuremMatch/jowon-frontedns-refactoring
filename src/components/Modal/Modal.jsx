89e8141aa6a2000c26c8c17c3bcbc9b576d2d82b
dongyu-youn <donghe1472@naver.com>
2024-09-03 18:40:03 +0900

    [Feat] About 글 수정

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -4,7 +4,7 @@ export function Modal({ isOpen, closeModal, children }) {
   if (!isOpen) return null;
 
   return (
-    <div className="fixed  inset-0 bg-black bg-opacity-50 flex items-center justify-center">
+    <div className="fixed  inset-0 bg-black bg-opacity-50 flex items-center justify-center mt-8">
       <div className="bg-white p-8 rounded-lg shadow-lg relative  w-full max-w-4xl  h-4/5 overflow-y-auto ">
         <button
           onClick={closeModal}
--- src/pages/Home/About.jsx
+++ src/pages/Home/About.jsx
@@ -8,20 +8,50 @@ export default function About({ scrollToSection }) {
     <Element name="about" className="bg-black p-24">
       <div className="flex">
         <img src="imgs/about.png" alt="" className="w-1/2 h-1/2" />
-        <div className="text-white text-lg flex items-center flex-col justify-center ml-8 font-diphylleia leading-10 text-center">
-          <div className="flex justify-center text-2xl font-bold mb-4 font-customFont">
-            steven jobs - "나의 최고의 작품은 팀이다" <br />
+        <div className="flex flex-col text-left">
+          <div className="text-white text-left text-lg flex items-center flex-col justify-center ml-8 font-diphylleia leading-10">
+            대학생활의 꽃{" "}
+            <p className="font-bold text-2xl inline">"프로젝트"</p>
+            <br />
+            진정한 대학생활은 학교에서 배운 내용으로 누군가를
+            <br />
+            <span className="font-bold text-2xl inline">
+              "도울 수 있을 때"
+            </span>{" "}
+            <span style={{ marginLeft: "0.5rem" }}>
+              보람을 느낀다고 생각합니다
+            </span>
+            <br />
+            <span>
+              하지만 저학년이거나 프로젝트 초보일 경우 기술 스택은 한정되어 있고
+            </span>
+            <span className="font-bold text-2xl inline">"완성된"</span>{" "}
+            <span> 프로젝트를 하기엔 한계가 있습니다</span>
+            <br />
+            <span>이를 위해 1조원앱을 통해 나의 부족한 기술 스택을</span>
+            <span className="font-bold text-2xl inline">
+              "보충해 줄 수 있는"
+            </span>{" "}
+            <span> 팀원을 찾을 수 있으며</span>
+            <br />
+            이로써 대학생활에서 여러분의 최고의 작품을 만들고 자랑해보세요
           </div>
-          이 홈페이지는 열정적인 활동을 하고 싶지만
-          <br /> 같이 뜻을 모을 사람이 부족한 사람들을 위한 사이트입니다
-          <br /> 팀원 매칭을 통해 자신에게 적합한 팀원들을 만나볼 수 있으며
-          <br />
-          뜻이 맞는 팀원들과 적합한 대회와 활동을 통해 최고의 프로젝트를
-          <br />
-          만들어주는 홈페이지 입니다 또한 여러 필터와 <br /> 카테고리를 통해
-          쉽고 빠르게 대회들을 찾아볼 수 있으며
-          <br /> 1조원 그래프를 통해 팀에서 부족한 스택인원을 멋지게 보충할수
-          있습니다.
+          <div className="flex justify-center font-bold mb-4 mt-24 font-customFont text-left">
+            Steven Jobs - "나의 최고의 작품은 팀이다. 훌륭한 팀 없이는 어떤 것도
+            이룰 수 없다." ("My greatest work is the team. Without a great team,
+            nothing can be accomplished.")
+            <br />
+            <br />
+            헨리 포드 (Henry Ford) "함께 모이는 것은 시작이고, 함께 머무는 것은
+            발전이며, 함께 일하는 것은 성공이다." ("Coming together is a
+            beginning, staying together is progress, and working together is
+            success.")
+            <br />
+            <br />
+            마이클 조던 (Michael Jordan) "재능은 경기를 이기게 하지만, 팀워크와
+            지성은 챔피언십을 이기게 한다." ("Talent wins games, but teamwork
+            and intelligence win championships.")
+          </div>
         </div>
       </div>
       <Scrolls />
